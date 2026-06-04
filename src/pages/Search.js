8d7af2b6a0c8ce456dbff125011d78f4a5d4d0e6
varshith-017 <varshithap03@gmail.com>
2026-06-04 21:03:48 +0530

    Update Search.js

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -8,15 +8,17 @@ export default function Search() {
 
   useEffect(() => {
     API.get(`/products/search/${key}`)
-      .then(res => setProducts(res.data));
+      .then((res) => setProducts(res.data))
+      .catch((err) => console.log(err));
   }, [key]);
 
   return (
-    <div>
+    <div className="products">
       <h2>Search Results</h2>
 
-      {products.map(p => (
-        <div key={p._id}>
+      {products.map((p) => (
+        <div key={p._id} className="card">
+          <img src={p.image} alt={p.name} />
           <h3>{p.name}</h3>
           <p>₹{p.price}</p>
         </div>
